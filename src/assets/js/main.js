c10c1881dc9d3beb28bc0cf4c55260dbd4acd040
office PC <teddywijaya93@gmail.com>
2023-03-28 13:18:57 +0700

    new ratu desktop done

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -184,7 +184,7 @@ const app = (() => {
 		$(".slide-product").slick({
 			dots: false,
 			infinite: true,
-			slidesToShow: 3,
+			slidesToShow: 1,
 			slidesToScroll: 1,
 			autoplay: true,
 			autoplaySpeed: 5000,
